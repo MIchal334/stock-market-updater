f7a34bc77a0d214073ac2b09d032a05087278f7b
Michał Mencfel <michal.mencfel@batteryenergyback.com>
2025-02-26 18:51:29 +0100

    feat: crate SellerService

--- adapter/outbound/sql_data_updtaer.py
+++ adapter/outbound/sql_data_updtaer.py
@@ -3,6 +3,14 @@ from application.port.outbound.data_updater import DataUpdater
 
 
 class SqlDataUpdater(DataUpdater):
+    def get_current_amount_of_action(self, user_email: str, company_name: str):
+        session = create_session()
+        record = session.query(OwnedStock).filter_by(customer_email=user_email, company_name=company_name).first()
+        if record is None:
+            record = 0
+
+        return record.action_amount
+
     def update_user_data(self, user_email: str, current_amount_of_action: int, company_name: str):
         session = create_session()
 
--- application/seller_service.py
+++ application/seller_service.py
@@ -3,6 +3,9 @@ from concurrent.futures import ThreadPoolExecutor
 from application.port.inbound.event_handler import EventHandler
 from application.port.outbound.data_updater import DataUpdater
 from application.port.outbound.user_informer import UserInformer
+from domain.event_info import EventInfo
+
+EMAIL_TEMPLATE = "Market info. Operation {}. Company name {}. Current amount of action: {}"
 
 
 class SellerService:
@@ -13,11 +16,26 @@ class SellerService:
 
     def check_events_occurrence(self):
         with ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(self.businesses_operation)
+            executor.submit(self.__businesses_operation)
 
-    def businesses_operation(self):
+    def __businesses_operation(self):
         event_to_process = self.event_handler.handle_event()
         if event_to_process is None:
             return
 
+        current_action_amount = self.data_updater.get_current_amount_of_action(user_email=event_to_process.client_email,
+                                                                               company_name=event_to_process.company_name)
 
+        action_amount_after_action = event_to_process.action.apply(current_action_amount, event_to_process.action_amount)
+
+        self.data_updater.update_user_data(user_email=event_to_process.client_email,
+                                           company_name=event_to_process.company_name,
+                                           current_amount_of_action=action_amount_after_action)
+
+        self.user_informer.send_info_to_user(user_email=event_to_process.client_email,
+                                             info=self.__create_information_for_user(event=event_to_process,
+                                                                                     action_amount_after_action=action_amount_after_action))
+
+    @staticmethod
+    def __create_information_for_user(event: EventInfo, action_amount_after_action: int) -> str:
+        return EMAIL_TEMPLATE.format(event.action.value, event.company_name, action_amount_after_action)
