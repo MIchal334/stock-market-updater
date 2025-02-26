9ef9b85336d113c99649f84e8826cd06e6112e35
Michał Mencfel <michal.mencfel@batteryenergyback.com>
2025-02-26 17:27:07 +0100

    feat: implement  "handle_event" for topic "incident_topic"

--- adapter/kafka_event_handler.py
+++ adapter/kafka_event_handler.py
@@ -6,9 +6,9 @@ from application.port.inbound.event_handler import EventHandler
 from domain.event_info import EventInfo
 
 KAFKA_SERVER_ADDRESS = getenv("KAFKA_SERVER_ADDRESS", "localhost:9092")
-KAFKA_CONSUMER_GROUP_NAME = getenv("KAFKA_CONSUMER_GROUP_NAME", "KAFKA_SERVER_ADDRESS")
+KAFKA_CONSUMER_GROUP_NAME = getenv("KAFKA_CONSUMER_GROUP_NAME", "consumer_group")
 KAFKA_OFFSET_MODE = getenv("KAFKA_OFFSET_MODE", "earliest")
-TOPIC_NAME = getenv("TOPIC_NAME", "stock_market")
+TOPIC_NAME = getenv("TOPIC_NAME", "incident_topic")
 
 CONF = {
     'bootstrap.servers': KAFKA_SERVER_ADDRESS,
@@ -25,14 +25,10 @@ class KafkaEventHandler(EventHandler):
     def handle_event(self) -> EventInfo:
         msg = self.kafka_consumer.poll(1.0)
 
-        if msg is None:
-            return None
-
-        if msg.error():
+        if msg is None or msg.error():
             return None
 
         message_value = msg.value().decode('utf-8')
-        print(f"Odebrano wiadomość: {message_value}")
 
         return EventInfo.of(message_value)
 
--- domain/event_info.py
+++ domain/event_info.py
@@ -8,9 +8,9 @@ class EventInfo:
     client_email: str
     company_name: str
     action: Action
-    actionAmount: int
+    action_amount: int
 
     @staticmethod
     def of(json_dict):
-        return EventInfo(client_email=json_dict['client_email'], company_name=json_dict['company_name'],
-                         action=Action.of(json_dict['action']), actionAmount=json_dict['actionAmount'])
+        return EventInfo(client_email=json_dict['clientEmail'], company_name=json_dict['companyName'],
+                         action=Action.of(json_dict['action']), action_amount=json_dict['actionAmount'])
